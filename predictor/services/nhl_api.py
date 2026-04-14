62bf405b9ee2676b625efefdaf0b037247a3e9de
hqluke <hqluke@users.noreply.github.com>
2026-04-14 17:44:24 -0600

    function to find the next game utah plays

--- predictor/services/nhl_api.py
+++ predictor/services/nhl_api.py
@@ -1,1 +1,23 @@
 import requests
+from datetime import datetime
+
+# get current date formatted as YYYY-MM-DD
+today = datetime.now().strftime("%Y-%m-%d")
+
+
+def get_next_game():
+    url = "https://api-web.nhle.com/v1/club-schedule/UTA/week/now"
+    response = requests.get(url)
+    games = response.json()["games"]
+    for game in games:
+        if game["gameDate"] >= today:
+            return game
+
+
+# print info as needed
+# call python predictor/services/nhl_api.py
+if __name__ == "__main__":
+    import json
+
+    game = get_next_game()
+    print(json.dumps(game, indent=2))
