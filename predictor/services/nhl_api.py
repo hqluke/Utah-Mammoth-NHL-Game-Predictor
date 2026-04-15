298f8b1aa48e205ffbfa3c2fd2fd4498fe7f7b0d
hqluke <hqluke@users.noreply.github.com>
2026-04-15 17:30:03 -0600

    Gather team ids for next game

--- predictor/services/nhl_api.py
+++ predictor/services/nhl_api.py
@@ -14,10 +14,29 @@ def get_next_game():
             return game
 
 
+def get_next_teams():
+    game = get_next_game()
+    away_team = game["awayTeam"]
+    home_team = game["homeTeam"]
+    if away_team["id"] == 68:
+        utah = away_team
+        opponent = home_team
+    else:
+        utah = home_team
+        opponent = away_team
+    utahID = utah["id"]
+    opponentID = opponent["id"]
+    # print(utah["logo"])
+    return utahID, opponentID
+
+
 # print info as needed
 # call python predictor/services/nhl_api.py
 if __name__ == "__main__":
     import json
 
-    game = get_next_game()
-    print(json.dumps(game, indent=2))
+    # game = get_next_game()
+    # print(json.dumps(game, indent=2))
+
+    utahId, opponentId = get_next_teams()
+    print(json.dumps([utahId, opponentId], indent=2))
